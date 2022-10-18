5cfbdf8c5b034947af7168ea9677cb9e7c281247
Jasoliya Brijesh <jasoliyabrijesh123@gmail.com>
2022-10-18 14:13:57 +0530

    changes

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { AccountLoginForm } from "./account/AccountLoginForm.client";
-export { AccountRecoverForm } from "./account/AccountRecoverForm.client";
-export { LogoutButton } from "./account/LogoutButton.client";
-export { AccountOrderHistory } from "./account/AccountOrderHistory.client";
-export { AccountDetails } from "./account/AccountDetails.client";
-export { AccountAddresses } from "./account/AccountAddresses.client";
-export { Layout } from "./Layout.server";
-export { Popup } from "./Popup.client";
